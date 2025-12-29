111a9dd93a309aae97dc4e13c1842f025cc1c954
amit kumar <amitkumarak207302@gmail.com>
2025-12-30 01:16:44 +0530

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/amitkumar/',   // 🔴 VERY IMPORTANT
+  base: "/amitkumar/",
   plugins: [react(), tailwindcss()],
-})
+});
